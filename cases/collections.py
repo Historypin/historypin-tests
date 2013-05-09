e7bb73f02e90cb1579cac54b3e209027fe1d61b5
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-05-09 16:28:59 +0300

    Collections.test_index

--- cases/collections.py
+++ cases/collections.py
@@ -5,30 +5,47 @@ from base import *
 class Collections(HPTestCase):
 	@url('/collections/')
 	def test_index(self):
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		pass
+		self.assertTitle('Historypin | Collections')
+		
+		main_cnt	= self.e('.col.w34')
+		self.assertEqual('What are Collections?'									, main_cnt.e('h1').text)
+		self.assertEqual(URL_BASE + '/collections/'									, main_cnt.e('a.main-image.no-shadow').get_attribute('href'))
+		self.assertEqual(URL_BASE + '/resources/images/collections_page_image.jpg'	, main_cnt.e('img').get_attribute('src'))
+		self.assertEqual('Collections bring together content around a particular topic or theme. You can explore the Collections or create a Collection of your own.',
+						main_cnt.e('p').text)
+		
+		button		= self.e('.col.w34 a.next-button.left')
+		self.assertEqual(URL_BASE + '/collections/add'	, button.get_attribute('href'))
+		self.assertEqual('Make your own collection'		, button.e('span').text)
+		
+		self.assertEqual('All Collections'				, self.e('h3:last-of-type').text)
+		
+		h3 = self.e('h3.right a')
+		self.assertEqual('Return to Tours & Collections', h3.text)
+		self.assertEqual(URL_BASE + '/curated'			, h3.get_attribute('href'))
+		
+		cnt	= self.es('#photo_list_content .list li:nth-of-type(1)')
+		self.assertIsInstance(cnt[0].e('a')	, WebElement)
+		self.assertIsInstance(cnt[0].e('img')	, WebElement)
+		self.assertIsInstance(cnt[0].e('p')	, WebElement)
+		
+		self.assertIn('collection-icon'	, cnt[0].e('a span').get_attribute('class'))
+		self.assertIn('ss-pictures'		, cnt[0].e('a span').get_attribute('class'))
+		self.assertIn('ss-icon'			, cnt[0].e('a span').get_attribute('class'))
 	
 	@url('/collections/all')
 	def test_all(self):
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
-		# 
+		# assert title
+		# what are collections 
+		# assert what are collections heading 
+		# assert what are collections heading 
+		# assert image link src
+		# assert paragraph
+		# assert button link and text
+		# assert All Collections and Return to... texts
+		# assert web elements img link text 
+		# assertIn collection icon
+		# assert next link text 
 		pass
 	
 	@url('/collections/view/id/7082165/title/Brooklyn%20Public%20Library%20Branches')
