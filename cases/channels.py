b61138fae2c207ac10a8f9bcc5fb817a4e89e177
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-05-20 13:35:16 +0300

    Channels.test_search_email

--- cases/channels.py
+++ cases/channels.py
@@ -27,6 +27,7 @@ class Channels(HPTestCase):
 		self.assertIsInstance(channel.e('a.name')	, WebElement)
 	
 	def __test_channel_assertion(self):
+		self.assertEqual('Search Results for "Gabss":', self.e('.search > h2').text)
 		channel = self.e('.channels-list li')
 		self.assertEqual(URL_BASE + '/channels/view/id/10649049/'						, channel.e('a.logo').get_attribute('href'))
 		self.assertEqual(URL_BASE + '/channels/img/10649049/logo/1/dim/70x70/crop/1/'	, channel.e('a.logo img').get_attribute('src'))
@@ -55,24 +56,15 @@ class Channels(HPTestCase):
 		sleep(1)
 		self.e('.button.left').click()
 		
-		h2 = self.e('.search-channels .right a')
+		self.assertEqual('Search Results for "g.ananieva@avalith.bg":', self.e('.search > h2').text)
+		
 		channel = self.e('.channels-list li')
-		info = ['/channels/view/id/16857003/', '/resources/avatars/100x100/avatar_1.png', 'gabriela.ananieva', 'Return to Featured Channels', '/channels/']
-		for n in range(len(info)):
-			self.assertEqual(URL_BASE + info[0]	, channel[n].e('a.logo').get_attribute('href'))
-			self.assertEqual(URL_BASE + info[0]	, channel[n].e('a.name').get_attribute('href'))
-			self.assertEqual(URL_BASE + info[1]	, channel[n].e('a.logo img').get_attribute('src'))
-			self.assertEqual(info[2]			, channel[n].e('a.name').text)
-			self.assertEqual(info[3]			, h2[n].text)
-			self.assertEqual(URL_BASE + info[4]	, h2[n].get_attribute('href'))
-			
-		# channel = self.e('.channels-list li')
-		# self.assertEqual(URL_BASE + '/channels/view/id/16857003/'				, channel.e('a.logo').get_attribute('href'))
-		# self.assertEqual(URL_BASE + '/resources/avatars/100x100/avatar_1.png'	, channel.e('a.logo img').get_attribute('src'))
-		# self.assertEqual('gabriela.ananieva'									, channel.e('a.name').text)
-		# self.assertEqual(URL_BASE + '/channels/view/id/16857003/'				, channel.e('a.name').get_attribute('href'))
-		# 
-		# h2 = self.e('.search-channels .right a')
-		# self.assertEqual('Return to Featured Channels'									, h2.text)
-		# self.assertEqual(URL_BASE + '/channels/'										, h2.get_attribute('href'))
+		self.assertEqual(URL_BASE + '/channels/view/id/16857003/'				, channel.e('a.logo').get_attribute('href'))
+		self.assertEqual(URL_BASE + '/resources/avatars/100x100/avatar_1.png'	, channel.e('a.logo img').get_attribute('src'))
+		self.assertEqual('gabriela.ananieva'									, channel.e('a.name').text)
+		self.assertEqual(URL_BASE + '/channels/view/id/16857003/'				, channel.e('a.name').get_attribute('href'))
+		
+		h2 = self.e('.search-channels .right a')
+		self.assertEqual('Return to Featured Channels'									, h2.text)
+		self.assertEqual(URL_BASE + '/channels/'										, h2.get_attribute('href'))
 	
