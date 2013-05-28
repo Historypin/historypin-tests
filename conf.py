f44fc24cccb3e9ffd7a6ca918f303b4e4a85197f
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-05-28 14:07:27 +0300

    cofings

--- conf.py
+++ conf.py
@@ -1,7 +1,9 @@
 import os
 
-# URL_BASE = 'http://v4-21-00.historypin-hrd.appspot.com/'
-URL_BASE = 'http://www.localhost.com:8080'
+URL_BASE = 'http://www.v4-22-00.historypin-hrd.appspot.com'
+# URL_BASE = 'http://www.karamfil.in:8080'
 
 PATH_CRHOME_DRIVER	= '/usr/lib64/chromium-browser/chromedriver'
 
+KEY_COLLECTION = '22782015'
+KEY_TOUR = '22354015'
