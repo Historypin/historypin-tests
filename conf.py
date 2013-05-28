e0636da54e4d919e86ddf59043fb348e581f6863
Alexander Ivanov - Karamfil <karamfil.pc@gmail.com>
2013-05-28 15:53:59 +0300

    config IS_ON_SDK

--- conf.py
+++ conf.py
@@ -2,6 +2,7 @@ import os
 
 URL_BASE = 'http://www.v4-22-00.historypin-hrd.appspot.com'
 # URL_BASE = 'http://www.karamfil.in:8080'
+IS_ON_SDK = not URL_BASE.endswith('.appspot.com')
 
 PATH_CRHOME_DRIVER	= '/usr/lib64/chromium-browser/chromedriver'
 
