f8c6b5a00e6466591991d3e16ef7d757bebe177a
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-09-02 17:38:23 +0300

    tests

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 import os
 
-LINK_BASE = 'v4-24-02.historypin-hrd.appspot.com'
+LINK_BASE = 'v4-25-00.historypin-hrd.appspot.com'
 URL_BASE = 'http://www.' + LINK_BASE
 URL_ATTACH = 'http://attach.uid11675544.' + LINK_BASE
 IS_ON_SDK = not URL_BASE.endswith('.appspot.com')
