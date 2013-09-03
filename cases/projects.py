e00db4814ac1b71ed172b19d5c49ded1444f5f56
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-09-03 15:50:53 +0300

    Test Projects.test_index projects added

--- cases/projects.py
+++ cases/projects.py
@@ -13,16 +13,17 @@ class Projects(HPTestCase):
 		self.assertEqual(URL_BASE + '/contact', paragraph.e('a').get_attribute('href'))
 		
 		projects = [
-			['11461010-hp-olympics'		, "Olympic memories"			, "Have you got photos and memories from the Olympics through the ages?"									, '11461010/type/banner_image'],
-			['11462012-DiamondJubilee'	, "Pinning The Queen's history"	, "Have you celebrated a Royal Jubilee over the past 60 years or even seen a Royal visit?"					, '11462012/type/banner_image'],
-			['11483012-balboa'			, "Balboa Park"					, "Celebrating the Centennial of the 1915-1916 Panama-California Exposition in San Diego, CA."				, '11483012/type/project_image'],
-			['11499008-chevy'			, "Me and my Chevy"				, "Share your memories of your first cars, family holidays, road trips and more."							, '11499008/type/banner_image'],
-			['11886013-grandparents'	, "Amazing Grandparents"		, "Is your Gran, Grandad, Nan or Pops awesome? Add your grandparents to our Hall of Fame."					, '11886013/type/banner_image'],
-			['13388066-remember'		, "Remember how we used to..."	, "work, play, watch and listen, cook and clean, keep warm and celebrate. How did you used to do things?"	, '13388066/type/banner_image'],
-			['13839007-yearofthebay'	, "Year of the Bay"				, "What events, big and small, have shaped the San Francisco Bay? Share your memories."						, '13839007/type/banner_image'],
-			['15742010-sandy'			, "Hurricane Sandy"				, "Record, Remember, Rebuild: How have communities in the US and Caribbean been affected by Sandy?"			, '15742010/type/project_image'],
-			['16690005-1989'			, "Europeana 1989"				, "Share your stories, photographs and videos about the Fall of the Iron Curtain."							, '16690005/type/project_image'],
-			['23580013-japan-project'	, u"Historypin 日本上陸！"		, u"日本での展開について詳しく知る"																				, '23580013/type/project_image'],
+			['11461010-hp-olympics'				, "Olympic memories"			, "Have you got photos and memories from the Olympics through the ages?"									, '11461010/type/banner_image'],
+			['11462012-DiamondJubilee'			, "Pinning The Queen's history"	, "Have you celebrated a Royal Jubilee over the past 60 years or even seen a Royal visit?"					, '11462012/type/banner_image'],
+			['11483012-balboa'					, "Balboa Park"					, "Celebrating the Centennial of the 1915-1916 Panama-California Exposition in San Diego, CA."				, '11483012/type/project_image'],
+			['11499008-chevy'					, "Me and my Chevy"				, "Share your memories of your first cars, family holidays, road trips and more."							, '11499008/type/banner_image'],
+			['11886013-grandparents'			, "Amazing Grandparents"		, "Is your Gran, Grandad, Nan or Pops awesome? Add your grandparents to our Hall of Fame."					, '11886013/type/banner_image'],
+			['13388066-remember'				, "Remember how we used to..."	, "work, play, watch and listen, cook and clean, keep warm and celebrate. How did you used to do things?"	, '13388066/type/banner_image'],
+			['13839007-yearofthebay'			, "Year of the Bay"				, "What events, big and small, have shaped the San Francisco Bay? Share your memories."						, '13839007/type/project_image'],
+			['15742010-sandy'					, "Hurricane Sandy"				, "Record, Remember, Rebuild: How have communities in the US and Caribbean been affected by Sandy?"			, '15742010/type/project_image'],
+			['16690005-1989'					, "Europeana 1989"				, "Share your stories, photographs and videos about the Fall of the Iron Curtain."							, '16690005/type/project_image'],
+			['23580013-japan-project'			, u"Historypin 日本上陸！"		,	 u"日本での展開について詳しく知る"																					, '23580013/type/project_image'],
+			['30128133-putting-art-on-the-map'	, 'Putting Art on the Map'		, 'Explore, curate and enrich the Imperial War Museums\' First World War artworks.'							, '30128133/type/project_image'],
 		]
 		
 		
