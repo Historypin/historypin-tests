1e9db53d3b4b917cf72dd47814ac498bb42e4187
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-10-31 16:59:58 +0200

    Test Project_Npower.test_celebrate and functions test_touts and test_channels for projects

--- cases/projects_cases/npower.py
+++ cases/projects_cases/npower.py
@@ -3,6 +3,65 @@
 from base import *
 
 class Project_NPower(HPTestCase):
+	
+	def __test_touts(self):
+		
+		site_cnt = self.e('#site-content')
+		
+		icon_tout1 = site_cnt.e('#icon-tout-0 a')
+		
+		self.assertEqual('%s/project/31-remember-timeline/' % URL_BASE, icon_tout1.get_attribute('href'))
+		self.assertEqual('Explore our timeline of inventions\nover the last century', icon_tout1.text)
+		self.assertIn('ss-icon'		, icon_tout1.e('span').get_attribute('class'))
+		self.assertIn('ss-activity'	, icon_tout1.e('span').get_attribute('class'))
+		
+		icon_tout2 = site_cnt.e('#icon-tout-1 a')
+		
+		self.assertEqual('http://blog.historypin.com/category/remember-how-we-used-to/'		, icon_tout2.get_attribute('href'))
+		self.assertEqual('Check out our blog for all the latest\nnews and community updates', icon_tout2.text)
+		self.assertIn('ss-icon'		, icon_tout2.e('span').get_attribute('class'))
+		self.assertIn('ss-openbook'	, icon_tout2.e('span').get_attribute('class'))
+		
+	def __test_channels(self):
+		
+		site_cnt = self.e('#site-content')
+		bottom_p = site_cnt.e('.bottom-p')
+		self.assertIn('Featured photos shared by', bottom_p.text)
+			
+		channels = [
+			['id/571038/'	, 'Mirrorpix'],
+			['8759065/'		, 'Dawn Parsonage'],
+			['id/8285366/'	, 'The Benevolent Society'],
+			['id/3036003/'	, 'Science and Society Picture Library'],
+			['id/6900604/'	, 'Reading Post'],
+			['id/14424002/'	, 'npower archive'],
+		]
+			
+		channels_links = bottom_p.es('a')
+		
+		for n in range(len(channels)):
+			i = channels[n]
+			self.assertEqual(URL_BASE + '/channels/view/' + i[0], channels_links[n].get_attribute('href'))
+			self.assertEqual(i[1], channels_links[n].text)
+		
+		last_paragraph = self.e('.bottom-p + p')
+		self.assertEqual('In partnership with', last_paragraph.e('span').text)
+		
+		
+		partners = [
+			['http://www.npower.com/'	, 'npower_logo.png'],
+			['http://www.mirrorpix.com/', 'mirrorpix.jpg'],
+		]
+		
+		links	= last_paragraph.es('a')
+		imgs	= last_paragraph.es('a img')
+		
+		for n in range(len(partners)):
+			i = partners[n]
+			self.assertEqual(i[0], links[n].get_attribute('href'))
+			self.assertEqual(URL_BASE + '/resources/images/webapps/npower/' + i[1], imgs[n].get_attribute('src'))
+		
+	
 	@url('/project/15-remember')
 	def test_index(self):
 		
@@ -55,57 +114,11 @@ class Project_NPower(HPTestCase):
 			self.assertEqual(URL_BASE + '/projects/img/pid/' + i[2] + '/type/project_image,banner_image/dim/320x144/crop/1/', imgs[n].get_attribute('src'))
 			self.assertIn(i[3], texts[n].text)
 		
-		icon_tout1 = site_cnt.e('#icon-tout-0 a')
-		
-		self.assertEqual('%s/project/31-remember-timeline/' % URL_BASE, icon_tout1.get_attribute('href'))
-		self.assertEqual('Explore our timeline of inventions\nover the last century', icon_tout1.text)
-		self.assertIn('ss-icon'		, icon_tout1.e('span').get_attribute('class'))
-		self.assertIn('ss-activity'	, icon_tout1.e('span').get_attribute('class'))
-		
-		icon_tout2 = site_cnt.e('#icon-tout-1 a')
-		
-		self.assertEqual('http://blog.historypin.com/category/remember-how-we-used-to/'		, icon_tout2.get_attribute('href'))
-		self.assertEqual('Check out our blog for all the latest\nnews and community updates', icon_tout2.text)
-		self.assertIn('ss-icon'		, icon_tout2.e('span').get_attribute('class'))
-		self.assertIn('ss-openbook'	, icon_tout2.e('span').get_attribute('class'))
+		self.__test_touts()
+		self.__test_channels()
 		
 		self.assertEqual('%s/attach/project/15-remember/photos/gallery/' % URL_BASE, self.e('#embed-frame').get_attribute('src'))
 		
-		bottom_p = site_cnt.e('.bottom-p')
-		self.assertIn('Featured photos shared by', bottom_p.text)
-		
-		channels = [
-			['id/571038/'	, 'Mirrorpix'],
-			['8759065/'		, 'Dawn Parsonage'],
-			['id/8285366/'	, 'The Benevolent Society'],
-			['id/3036003/'	, 'Science and Society Picture Library'],
-			['id/6900604/'	, 'Reading Post'],
-			['id/14424002/'	, 'npower archive'],
-		]
-		
-		channels_links = bottom_p.es('a')
-		
-		for n in range(len(channels)):
-			i = channels[n]
-			self.assertEqual(URL_BASE + '/channels/view/' + i[0], channels_links[n].get_attribute('href'))
-			self.assertEqual(i[1], channels_links[n].text)
-		
-		last_paragraph = self.e('.bottom-p + p')
-		self.assertEqual('In partnership with', last_paragraph.e('span').text)
-		
-		
-		partners = [
-			['http://www.npower.com/'	, 'npower_logo.png'],
-			['http://www.mirrorpix.com/', 'mirrorpix.jpg'],
-		]
-		
-		links	= last_paragraph.es('a')
-		imgs	= last_paragraph.es('a img')
-		
-		for n in range(len(partners)):
-			i = partners[n]
-			self.assertEqual(i[0], links[n].get_attribute('href'))
-			self.assertEqual(URL_BASE + '/resources/images/webapps/npower/' + i[1], imgs[n].get_attribute('src'))
 	
 	@url('/project/16-remember-keep-warm')
 	def test_keep_warm(self):
@@ -147,19 +160,8 @@ class Project_NPower(HPTestCase):
 			self.assertEqual(URL_BASE + '/project/' + i[1], img_links[n].get_attribute('href'))
 			self.assertEqual(URL_BASE + '/projects/img/pid/' + i[2] + '/type/project_image,banner_image/dim/320x144/crop/1/', imgs[n].get_attribute('src'))
 		
-		icon_tout1 = site_cnt.e('#icon-tout-0 a')
-		
-		self.assertEqual('%s/project/31-remember-timeline/' % URL_BASE, icon_tout1.get_attribute('href'))
-		self.assertEqual('Explore our timeline of inventions\nover the last century', icon_tout1.text)
-		self.assertIn('ss-icon'		, icon_tout1.e('span').get_attribute('class'))
-		self.assertIn('ss-activity'	, icon_tout1.e('span').get_attribute('class'))
-		
-		icon_tout2 = site_cnt.e('#icon-tout-1 a')
-		
-		self.assertEqual('http://blog.historypin.com/category/remember-how-we-used-to/'		, icon_tout2.get_attribute('href'))
-		self.assertEqual('Check out our blog for all the latest\nnews and community updates', icon_tout2.text)
-		self.assertIn('ss-icon'		, icon_tout2.e('span').get_attribute('class'))
-		self.assertIn('ss-openbook'	, icon_tout2.e('span').get_attribute('class'))
+		self.__test_touts()
+		self.__test_channels()
 		
 		self.assertEqual('%s/attach/project/16-remember-keep-warm/photos/gallery/' % URL_BASE, self.e('#embed-frame').get_attribute('src'))
 		
@@ -204,19 +206,8 @@ class Project_NPower(HPTestCase):
 			self.assertEqual(URL_BASE + '/project/' + i[1], img_links[n].get_attribute('href'))
 			self.assertEqual(URL_BASE + '/projects/img/pid/' + i[2] + '/type/project_image,banner_image/dim/320x144/crop/1/', imgs[n].get_attribute('src'))
 		
-		icon_tout1 = site_cnt.e('#icon-tout-0 a')
-		
-		self.assertEqual('%s/project/31-remember-timeline/' % URL_BASE, icon_tout1.get_attribute('href'))
-		self.assertEqual('Explore our timeline of inventions\nover the last century', icon_tout1.text)
-		self.assertIn('ss-icon'		, icon_tout1.e('span').get_attribute('class'))
-		self.assertIn('ss-activity'	, icon_tout1.e('span').get_attribute('class'))
-		
-		icon_tout2 = site_cnt.e('#icon-tout-1 a')
-		
-		self.assertEqual('http://blog.historypin.com/category/remember-how-we-used-to/'		, icon_tout2.get_attribute('href'))
-		self.assertEqual('Check out our blog for all the latest\nnews and community updates', icon_tout2.text)
-		self.assertIn('ss-icon'		, icon_tout2.e('span').get_attribute('class'))
-		self.assertIn('ss-openbook'	, icon_tout2.e('span').get_attribute('class'))
+		self.__test_touts()
+		self.__test_channels()
 		
 		self.assertEqual('%s/attach/project/18-remember-play/photos/gallery/' % URL_BASE, self.e('#embed-frame').get_attribute('src'))
 		
@@ -249,6 +240,52 @@ class Project_NPower(HPTestCase):
 			['Work'				, '24-remember-work/'				, '24'],
 		]
 		
+		h2s			= self.es('.w5 h2')
+		h2s_links	= self.es('.w5 h2 a')
+		img_links	= self.es('.w5 a:nth-child(2)')
+		imgs		= self.es('.w5 a:nth-child(2) img')
+		
+		for n in range(len(projects)):
+			i = projects[n]
+			self.assertEqual(i[0], h2s[n].text)
+			self.assertEqual(URL_BASE + '/project/' + i[1], h2s_links[n].get_attribute('href'))
+			self.assertEqual(URL_BASE + '/project/' + i[1], img_links[n].get_attribute('href'))
+			self.assertEqual(URL_BASE + '/projects/img/pid/' + i[2] + '/type/project_image,banner_image/dim/320x144/crop/1/', imgs[n].get_attribute('src'))
+		
+		self.__test_touts()
+		self.__test_channels()
+		
+		self.assertEqual('%s/attach/project/19-remember-cook-and-clean/photos/gallery/' % URL_BASE, self.e('#embed-frame').get_attribute('src'))
+		
+	
+	@url('/project/21-remember-celebrate')
+	def test_celebrate(self):
+		
+		self.assertTitle('Celebrate | Home')
+		
+		site_cnt = self.e('#site-content')
+		desc = site_cnt.e('.right > a')
+		
+		self.assertEqual('%s/project/15-remember' % URL_BASE						, desc.get_attribute('href'))
+		self.assertEqual('%s/projects/img/pid/21/type/logo/dim/600x120/' % URL_BASE	, desc.e('img').get_attribute('src'))
+		
+		self.assertEqual('Dancing, decorations, parties! The bright lights of Christmas, bonfires and fireworks, wedding snaps with dodgy dos and birthdays from our youth.', site_cnt.e('.right p').text)
+		
+		button_upload = site_cnt.e('.left a')
+		
+		self.assertEqual('%s/project/15-remember/upload/projects/bridge/1/?subproject=21' % URL_BASE, button_upload.get_attribute('href'))
+		self.assertEqual('Pin your memories'														, button_upload.e('span').text)
+		
+		self.assertEqual('Explore more', site_cnt.e('.cf h3').text)
+		
+		projects = [
+			['Keep Warm'		, '16-remember-keep-warm/'			, '16'],
+			['Play'				, '18-remember-play/'				, '18'],
+			['Cook and Clean'	, '19-remember-cook-and-clean/'		, '19'],
+			['Watch and Listen'	, '23-remember-watch-and-listen/'	, '23'],
+			['Work'				, '24-remember-work/'				, '24'],
+		]
+		
 		h2s = self.es('.w5 h2')
 		h2s_links = self.es('.w5 h2 a')
 		img_links = self.es('.w5 a:nth-child(2)')
@@ -261,26 +298,11 @@ class Project_NPower(HPTestCase):
 			self.assertEqual(URL_BASE + '/project/' + i[1], img_links[n].get_attribute('href'))
 			self.assertEqual(URL_BASE + '/projects/img/pid/' + i[2] + '/type/project_image,banner_image/dim/320x144/crop/1/', imgs[n].get_attribute('src'))
 		
-		icon_tout1 = site_cnt.e('#icon-tout-0 a')
+		self.__test_touts()
+		self.__test_channels()
 		
-		self.assertEqual('%s/project/31-remember-timeline/' % URL_BASE, icon_tout1.get_attribute('href'))
-		self.assertEqual('Explore our timeline of inventions\nover the last century', icon_tout1.text)
-		self.assertIn('ss-icon'		, icon_tout1.e('span').get_attribute('class'))
-		self.assertIn('ss-activity'	, icon_tout1.e('span').get_attribute('class'))
+		self.assertEqual('%s/attach/project/21-remember-celebrate/photos/gallery/' % URL_BASE, self.e('#embed-frame').get_attribute('src'))
 		
-		icon_tout2 = site_cnt.e('#icon-tout-1 a')
-		
-		self.assertEqual('http://blog.historypin.com/category/remember-how-we-used-to/'		, icon_tout2.get_attribute('href'))
-		self.assertEqual('Check out our blog for all the latest\nnews and community updates', icon_tout2.text)
-		self.assertIn('ss-icon'		, icon_tout2.e('span').get_attribute('class'))
-		self.assertIn('ss-openbook'	, icon_tout2.e('span').get_attribute('class'))
-		
-		self.assertEqual('%s/attach/project/19-remember-cook-and-clean/photos/gallery/' % URL_BASE, self.e('#embed-frame').get_attribute('src'))
-		
-	
-	@url('/project/21-remember-celebrate')
-	def test_celebrate(self):
-		pass
 	
 	@url('/project/23-remember-watch-and-listen')
 	def test_watch_listen(self):
