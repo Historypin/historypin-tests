89d50c749fbfd2ee36d86e475c5b62470686a784
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-10-31 15:07:19 +0200

    Test Project_Npower.test_cook_clean

--- cases/projects_cases/npower.py
+++ cases/projects_cases/npower.py
@@ -223,7 +223,60 @@ class Project_NPower(HPTestCase):
 	
 	@url('/project/19-remember-cook-and-clean')
 	def test_cook_clean(self):
-		pass
+		
+		self.assertTitle('Cook and Clean | Home')
+		
+		site_cnt = self.e('#site-content')
+		desc = site_cnt.e('.right > a')
+		
+		self.assertEqual('%s/project/15-remember' % URL_BASE						, desc.get_attribute('href'))
+		self.assertEqual('%s/projects/img/pid/19/type/logo/dim/600x120/' % URL_BASE	, desc.e('img').get_attribute('src'))
+		
+		self.assertEqual('Dirty dishes in the days before dishwashers, cooking up a feast with all the family, arduous chores and the legends that did them.', site_cnt.e('.right p').text)
+		
+		button_upload = site_cnt.e('.left a')
+		
+		self.assertEqual('%s/project/15-remember/upload/projects/bridge/1/?subproject=19' % URL_BASE, button_upload.get_attribute('href'))
+		self.assertEqual('Pin your memories'														, button_upload.e('span').text)
+		
+		self.assertEqual('Explore more', site_cnt.e('.cf h3').text)
+		
+		projects = [
+			['Keep Warm'		, '16-remember-keep-warm/'			, '16'],
+			['Play'				, '18-remember-play/'				, '18'],
+			['Celebrate'		, '21-remember-celebrate/'			, '21'],
+			['Watch and Listen'	, '23-remember-watch-and-listen/'	, '23'],
+			['Work'				, '24-remember-work/'				, '24'],
+		]
+		
+		h2s = self.es('.w5 h2')
+		h2s_links = self.es('.w5 h2 a')
+		img_links = self.es('.w5 a:nth-child(2)')
+		imgs = self.es('.w5 a:nth-child(2) img')
+		
+		for n in range(len(projects)):
+			i = projects[n]
+			self.assertEqual(i[0], h2s[n].text)
+			self.assertEqual(URL_BASE + '/project/' + i[1], h2s_links[n].get_attribute('href'))
+			self.assertEqual(URL_BASE + '/project/' + i[1], img_links[n].get_attribute('href'))
+			self.assertEqual(URL_BASE + '/projects/img/pid/' + i[2] + '/type/project_image,banner_image/dim/320x144/crop/1/', imgs[n].get_attribute('src'))
+		
+		icon_tout1 = site_cnt.e('#icon-tout-0 a')
+		
+		self.assertEqual('%s/project/31-remember-timeline/' % URL_BASE, icon_tout1.get_attribute('href'))
+		self.assertEqual('Explore our timeline of inventions\nover the last century', icon_tout1.text)
+		self.assertIn('ss-icon'		, icon_tout1.e('span').get_attribute('class'))
+		self.assertIn('ss-activity'	, icon_tout1.e('span').get_attribute('class'))
+		
+		icon_tout2 = site_cnt.e('#icon-tout-1 a')
+		
+		self.assertEqual('http://blog.historypin.com/category/remember-how-we-used-to/'		, icon_tout2.get_attribute('href'))
+		self.assertEqual('Check out our blog for all the latest\nnews and community updates', icon_tout2.text)
+		self.assertIn('ss-icon'		, icon_tout2.e('span').get_attribute('class'))
+		self.assertIn('ss-openbook'	, icon_tout2.e('span').get_attribute('class'))
+		
+		self.assertEqual('%s/attach/project/19-remember-cook-and-clean/photos/gallery/' % URL_BASE, self.e('#embed-frame').get_attribute('src'))
+		
 	
 	@url('/project/21-remember-celebrate')
 	def test_celebrate(self):
