ea4bd0b6c141c2c0141c2ab55e90c65c180c319b
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-11-21 16:04:29 +0200

    PROJECT_URL in Test Project_AllOurStories

--- cases/projects_cases/allourstories.py
+++ cases/projects_cases/allourstories.py
@@ -16,14 +16,13 @@ class Project_AllOurStories(HPTestCase, Attach):
 	test_tab_gallery	= Attach.attach_tab_gallery
 	test_tab_slideshow	= Attach.attach_tab_slideshow
 	
-	@url(PROJECT_URL)
 	def test_index(self):
+		self.go(self.PROJECT_URL)
 		
 		self.assertTitle('All Our Stories | Home')
 		
 		self.assertEqual(URL_BASE + '/projects/img/dim/1000x250/crop/1/image_id/146', self.e('#banner_images img').get_attribute('src'))
 		
-		proj_link = '%s/project/44-all-our-stories' % URL_BASE
 		site_cnt = self.e('#site-content')
 		desc_main = site_cnt.e('.main_description')
 		
@@ -31,7 +30,7 @@ class Project_AllOurStories(HPTestCase, Attach):
 		
 		sleep(2)
 		button_proj = desc_main.e('.button.left')
-		self.assertEqual('%s/channels/'	% proj_link, button_proj.get_attribute('href'))
+		self.assertEqual('%s%s/channels/' % (URL_BASE, self.PROJECT_URL), button_proj.get_attribute('href'))
 		self.assertEqual('All Our Stories projects', button_proj.text)
 		
 		link_section = site_cnt.e('.additional-links')
@@ -42,8 +41,8 @@ class Project_AllOurStories(HPTestCase, Attach):
 		
 		hrefs = [
 			['https://s3-eu-west-1.amazonaws.com/wawwd-resources/HLF_AOS_Historypin+Guide_Final.pdf', 'Read the Get Started Guide'],
-			['%s/user/?from=/project/44-all-our-stories/channels/set_type/'				% proj_link	, 'Register'],
-			['%s/upload/'																% proj_link	, 'Upload your digital records'],
+			['%s%s/user/?from=%s/channels/set_type/' % (URL_BASE, self.PROJECT_URL, self.PROJECT_URL), 'Register'],
+			['%s%s/upload/'							 % (URL_BASE, self.PROJECT_URL), 'Upload your digital records'],
 		]
 		
 		for n in range(len(hrefs)):
@@ -65,13 +64,13 @@ class Project_AllOurStories(HPTestCase, Attach):
 		
 		self.assertEqual('%s/attach/project/44-all-our-stories/map/index/' % URL_BASE, self.e('#embed-frame').get_attribute('src'))
 	
-	@url(PROJECT_URL + '/channels/')
 	def test_projects(self):
+		self.go('%s/channels/' % self.PROJECT_URL)
 		
 		self.assertTitle('All Our Stories | Featured Channels')
 		
 		button_home = self.e('.button-wrapper a')
-		self.assertEqual('%s/project/44-all-our-stories/' % URL_BASE, button_home.get_attribute('href'))
+		self.assertEqual('%s%s/' % (URL_BASE, self.PROJECT_URL), button_home.get_attribute('href'))
 		self.assertEqual('Home', button_home.text)
 		
 		channel = self.e('.channels-list li:first-of-type')
