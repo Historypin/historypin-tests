889199b02dca769863920c64090ec3d629a28e70
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-11-22 14:33:23 +0200

    Test Project_During_Sandy

--- cases/__init__.py
+++ cases/__init__.py
@@ -32,6 +32,7 @@ from projects_cases.ptqh import Project_PTQH
 from projects_cases.putting_art import Project_PuttingArt
 from projects_cases.sandy import Project_Sandy
 from projects_cases.sandy_before import Project_Before_Sandy
+from projects_cases.sandy_during import Project_During_Sandy
 
 from projects_cases.yotb import Project_YOTB
 from projects_cases.yarra import Project_Yarra
