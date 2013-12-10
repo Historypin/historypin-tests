1412897c209523aab2d4d2038d3244060b9e7953
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-12-10 16:12:43 +0200

    Tests Project_Fujinomiya

--- cases/projects_cases/fujinomiya.py
+++ cases/projects_cases/fujinomiya.py
@@ -1,69 +1,66 @@
 # -*- coding: utf-8 -*-
 
 from base import *
-# from attach import Attach
+from attach import Attach
 
-class Project_Fujinomiya(HPTestCase):
+class Project_Fujinomiya(HPTestCase, Attach):
 	
-	JAPAN_URL	= 'http://www.historypin.jp'
-	PROJECT_URL	= '%s/jp/project/47-fujinomiya-project' % JAPAN_URL
-	# JAPAN_URL	= 'http://www.historypin.jp'
-	# PROJECT_URL	= '%s/project/47-fujinomiya-project' % JAPAN_URL
-	# ATTACH_URL = '/jp/attach'
+	PROJECT_URL	= '/project/47-fujinomiya-project'
+	ATTACH_URL	= '/jp/attach'
+	blog_link	= 'http://blog.historypin.jp'
 	
-	# ATTACH_TABS = [
-	# 	['%s/map/index/' % (PROJECT_URL), '%s/photos/gallery/' % PROJECT_URL],
-	# ]
+	ATTACH_TABS = [
+		['%s/map/index/' % (PROJECT_URL), '%s/photos/gallery/' % PROJECT_URL],
+	]
 	
-	# test_attach_tabs	= Attach.attach_tabs
-	# test_tab_map		= Attach.attach_tab_map
-	# test_tab_gallery	= Attach.attach_tab_gallery
+	test_attach_tabs	= Attach.attach_tabs
+	# test_tab_map		= Attach.attach_tab_map #the test will fail because there is only one photo on the map we seek for cluster
+	test_tab_gallery	= Attach.attach_tab_gallery
 	
 	def test_index(self):
 		self.go(self.PROJECT_URL)
 		
 		self.assertTitle('Fujinomiya project')
-		blog_link = 'http://blog.historypin.jp'
 		
 		site_cnt = self.e('#site')
 		nav_links = site_cnt.es('.primary a')
 		
-		self.assertEqual('%s/resources/images/project-japan/historypin-logo.png' % self.JAPAN_URL, nav_links[0].e('img').get_attribute('src'))
+		self.assertEqual('%s/resources/images/project-japan/historypin-logo.png' % URL_BASE, nav_links[0].e('img').get_attribute('src'))
 		self.assertEqual('http://www.historypin.com/', nav_links[0].get_attribute('href'))
 		
-		self.assertEqual('%s/' % self.PROJECT_URL, nav_links[1].get_attribute('href'))
+		self.assertEqual('%s/jp%s/' % (URL_BASE, self.PROJECT_URL), nav_links[1].get_attribute('href'))
 		self.assertEqual(u'ホーム', nav_links[1].text)
 		
-		self.assertEqual('%s/explore/#|map/' % self.PROJECT_URL, nav_links[2].get_attribute('href'))
+		self.assertEqual('%s/jp%s/explore/#|map/' % (URL_BASE, self.PROJECT_URL), nav_links[2].get_attribute('href'))
 		self.assertEqual(u'探索', nav_links[2].text)
 		
-		self.assertEqual('%s/upload/' % self.PROJECT_URL, nav_links[3].get_attribute('href'))
+		self.assertEqual('%s/jp%s/upload/' % (URL_BASE, self.PROJECT_URL), nav_links[3].get_attribute('href'))
 		self.assertEqual(u'投稿', nav_links[3].text)
 		
-		self.assertEqual('%s/category/historypin-japan/fujinomiya/' % blog_link, nav_links[4].get_attribute('href'))
+		self.assertEqual('%s/category/historypin-japan/fujinomiya/' % self.blog_link, nav_links[4].get_attribute('href'))
 		self.assertEqual(u'ブログ', nav_links[4].text)
 		
 		self.assertEqual('http://www.glocom.ac.jp/project/historypin/fujinomiya/', nav_links[5].get_attribute('href'))
 		self.assertEqual(u'富士宮プロジェクトについて', nav_links[5].text)
 		
-		self.assertEqual('%s/faq/' % blog_link		, nav_links[6].get_attribute('href'))
+		self.assertEqual('%s/faq/' % self.blog_link		, nav_links[6].get_attribute('href'))
 		self.assertEqual(u'よくある質問', nav_links[6].text)
 		
 		user_links = site_cnt.es('.secondary a')
 		
-		self.assertEqual('%s/user/' % self.PROJECT_URL, user_links[0].get_attribute('href'))
+		self.assertEqual('%s/jp%s/user/' % (URL_BASE, self.PROJECT_URL), user_links[0].get_attribute('href'))
 		self.assertEqual(u'アカウント作成', user_links[0].text)
 		
-		self.assertEqual('%s/user/' % self.PROJECT_URL, user_links[1].get_attribute('href'))
+		self.assertEqual('%s/jp%s/user/' % (URL_BASE, self.PROJECT_URL), user_links[1].get_attribute('href'))
 		self.assertEqual(u'ログイン', user_links[1].text)
 		
 		self.assertEqual(u'富士宮プロジェクト', self.e('.sec-header h1').text)
 		
-		self.assertEqual('%s/jp/attach/project/47-fujinomiya-project/map/index/' % self.JAPAN_URL, self.e('#embed-frame').get_attribute('src'))
+		self.assertEqual('%s/jp/attach%s/map/index/' % (URL_BASE, self.PROJECT_URL), self.e('#embed-frame').get_attribute('src'))
 		
 		touts_items = [
-			['http://www.glocom.ac.jp/project/historypin/fujinomiya/'			, u'富士宮プロジェクトについて（外部サイト）'	, u'富士宮プロジェクトについて詳しく知る'	, 'tout1_image'],
-			['http://blog.historypin.jp/category/historypin-japan/fujinomiya/'	, u'富士宮プロジェクトブログ'				, u'富士宮プロジェクトの最新情報'		, 'tout2_image'],
+			['http://www.glocom.ac.jp/project/historypin/fujinomiya/'	, u'富士宮プロジェクトについて（外部サイト）'	, u'富士宮プロジェクトについて詳しく知る'	, 'tout1_image'],
+			['%s/category/historypin-japan/fujinomiya/' % self.blog_link, u'富士宮プロジェクトブログ'				, u'富士宮プロジェクトの最新情報'			, 'tout2_image'],
 		]
 		
 		h3s_links	= self.es('.w23 .inner h3 a')
@@ -77,7 +74,7 @@ class Project_Fujinomiya(HPTestCase):
 			self.assertEqual(i[0], imgs_links[n].get_attribute('href'))
 			self.assertEqual(i[1], h3s_links[n].text)
 			self.assertEqual(i[2], paragraphs[n].text)
-			self.assertEqual(self.JAPAN_URL + '/projects/img/pid/47/dim/285x290/type/' + i[3] + '/crop/1/', imgs[n].get_attribute('src'))
+			self.assertEqual(URL_BASE + '/projects/img/pid/47/dim/285x290/type/' + i[3] + '/crop/1/', imgs[n].get_attribute('src'))
 		
 		activity = self.e('#activity')
 		self.assertIsInstance(activity.e('h1'), WebElement)
@@ -103,7 +100,7 @@ class Project_Fujinomiya(HPTestCase):
 		self.assertIn('ss-search'	, icon_tout2.e('span').get_attribute('class'))
 		
 		supported = self.e('.footer .supported_by span')
-		self.assertEqual('%s/fujinomiya/about/' % blog_link, supported.e('a').get_attribute('href'))
+		self.assertEqual('%s/fujinomiya/about/' % self.blog_link, supported.e('a').get_attribute('href'))
 		self.assertEqual(u'富士宮プロジェクト実行委員会', supported.e('a').text)
 		
 		self.assertEqual(u'使用写真はhalsan2000さんとtajima.tomocomさんがシェアしたものです。', self.e('.photos-by span').text)
@@ -112,9 +109,9 @@ class Project_Fujinomiya(HPTestCase):
 		footer_links = footer.es('a')
 		
 		footer_items = [
-			['%s/terms-and-conditions/'	% blog_link	, 'Terms and conditions'],
-			['%s/privacy-policy/'		% blog_link	, 'Privacy policy'],
-			['%s/cookies/'				% blog_link	, 'Cookies'],
+			['%s/terms-and-conditions/'	% self.blog_link	, 'Terms and conditions'],
+			['%s/privacy-policy/'		% self.blog_link	, 'Privacy policy'],
+			['%s/cookies/'				% self.blog_link	, 'Cookies'],
 			['http://wearewhatwedo.org/'			, u'© We Are What We Do'],
 		]
 		
