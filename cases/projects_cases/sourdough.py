00de7451f5c213d55eebf123870f6a3833fb76a4
Gabriela Ananieva <g.ananieva@avalith.bg>
2013-12-10 15:39:18 +0200

    Test Project_Sourdough.test_events and test_about

--- cases/projects_cases/sourdough.py
+++ cases/projects_cases/sourdough.py
@@ -64,25 +64,63 @@ class Project_Sourdough(HPTestCase):
 			i = supported_items[n]
 			self.assertEqual(i[0], supported_links[n].get_attribute('href'))
 			self.assertEqual('http://wawwd-resources.s3.amazonaws.com/historypin/projects/sandr/' + i[1], supported_imgs[n].get_attribute('src'))
-			
+		
 	
 	def test_about(self):
 		self.go(self.PROJECT_URL + '/about/')
-		# TODO
-		# assert title
-		# assert nav
-		# assert text
-		# assert image
+		
+		self.assertTitle('Sourdough and Rye')
+		
+		nav_items = [
+			['%s%s/'		% (URL_BASE, self.PROJECT_URL), 'Home'],
+			['%s%s/about'	% (URL_BASE, self.PROJECT_URL), 'About'],
+			['%s%s/explore'	% (URL_BASE, self.PROJECT_URL), 'Explore'],
+			['%s%s/upload'	% (URL_BASE, self.PROJECT_URL), 'Contribute'],
+			['%s%s/events'	% (URL_BASE, self.PROJECT_URL), 'Events'],
+			['http://blog.historypin.com/category/sourdough-rye/', 'Blog'],
+		]
+		
+		site_cnt = self.e('#site-content')
+		local_nav = site_cnt.e('.localnav')
+		nav_links = local_nav.es('a')
+		
+		for n in range(len(nav_items)):
+			i = nav_items[n]
+			self.assertEqual(i[0], nav_links[n].get_attribute('href'))
+			self.assertEqual(i[1], nav_links[n].text)
+		
+		self.assertIn('Like the great delis that once populated the Fillmore District, Sourdough & Rye is the place for Bay Area Jews', site_cnt.e('.about-inner p').text)
+		self.assertEqual('http://wawwd-resources.s3.amazonaws.com/historypin/projects/sandr/bread.jpg', site_cnt.e('#about-video').get_attribute('src'))
 	
 	def test_explore(self):
 		self.go(self.PROJECT_URL + '/explore#|map/')
-		# TODO
-		# assert title
-		# assert embed frame
 		
+		self.assertTitle('Sourdough and Rye')
+		self.assertEqual('%s/attach%s/map/' % (URL_BASE, self.PROJECT_URL), self.e('#embed-frame').get_attribute('src'))
+		
+	
 	def test_events(self):
 		self.go(self.PROJECT_URL + '/events/')
-		# TODO
-		# assert title
-		# assert links images and paragraphs
+		
+		site_cnt	= self.e('#site-content')
+		touts		= site_cnt.e('.image-links')
+		img_links	= touts.es('.item > a')
+		imgs		= touts.es('.item > a img')
+		h3s_links	= touts.es('h3 a')
+		paragraphs	= touts.es('p')
+		
+		touts_items = [
+			['/#', 'tout1_image', 'Upcoming events'		, 'More information coming soon!'],
+			['/#', 'tout2_image', 'Past events'			, 'More information coming soon!'],
+			['/#', 'tout3_image', 'Run your own event'	, 'More information coming soon!'],
+		]
+		
+		for n in range(len(touts_items)):
+			i = touts_items[n]
+			self.assertEqual(URL_BASE + self.PROJECT_URL + '/events' + i[0], img_links[n].get_attribute('href'))
+			self.assertEqual(URL_BASE + self.PROJECT_URL + '/events' + i[0], h3s_links[n].get_attribute('href'))
+			self.assertEqual(URL_BASE + '/projects/img/pid/43/dim/307x265/type/' + i[1] + '/crop/1/', imgs[n].get_attribute('src'))
+			self.assertEqual(i[2], h3s_links[n].text)
+			self.assertEqual(i[3], paragraphs[n].text)
+		
 	
