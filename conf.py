4c3259a8cd8b2a12886170ec3d383ba06a57dd26
Gabriela Ananieva <g.ananieva@avalith.bg>
2014-04-30 12:10:57 +0300

    Test Channel.test_upload_avatar fixed

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@ GO_TIMEOUT = 1
 # VERSION = 'v5-europeana-filter'
 # VERSION = 'v5-asian-pacific'
 VERSION = 'v6-alpha-4'
-# VERSION = 'v5-02-07'
+# VERSION = 'v5-02-09'
 # LINK_BASE = '%s.historypin-hrd.appspot.com' % (VERSION)
 LINK_BASE = 'historypin.com'
 
