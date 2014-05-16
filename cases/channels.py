10047834e1368d1eca4624dc9fba3ee1840f1ff7
Gabriela Ananieva <g.ananieva@avalith.bg>
2014-05-16 14:18:49 +0300

    fixed from channel to profile

--- cases/channels.py
+++ cases/channels.py
@@ -6,20 +6,20 @@ class Channels(HPTestCase):
 	
 	@url('/channels/')
 	def test_index(self):
-		self.assertTitle('Historypin | Featured Channels')
+		self.assertTitle('Historypin | Featured Profiles')
 		
 		main_img = self.e('.main-image')
 		self.assertEqual('%s/channels/#'							 % URL_BASE, main_img.get_attribute('href'))
 		self.assertEqual('%s/resources/images/channels/channels.jpg' % URL_BASE, main_img.e('img').get_attribute('src'))
 		
-		self.assertEqual('Historypin Channels', self.e('.info h1').text)
+		self.assertEqual('Historypin Profiles', self.e('.info h1').text)
 		
 		paragraph = self.es('.info p')
-		self.assertEqual("Channels hold everything someone has added to Historypin, including any Tours and Collections they've created, stories other people have added to their content and any Historypin Repeats of their images.", paragraph[0].text)
-		self.assertEqual("Remember: Channels are personalisable so can be re-designed to look just how you want them!", paragraph[1].text)
+		self.assertEqual("Profiles hold everything someone has added to Historypin, including any Tours and Collections they've created, stories other people have added to their content and any Historypin Repeats of their images.", paragraph[0].text)
+		self.assertEqual("Remember: Profiles are personalisable so can be re-designed to look just how you want them!", paragraph[1].text)
 		
-		self.assertEqual("Search Channel Names"		, self.e('.search-channels h2').text)
-		self.assertEqual("Some Featured Channels"	, self.e('.search h2').text)
+		self.assertEqual("Search Profile Names"		, self.e('.search-channels h2').text)
+		self.assertEqual("Some Featured Profiles"	, self.e('.search h2').text)
 		
 		channel = self.e('.channels-list li')
 		self.assertIsInstance(channel.e('img')		, WebElement)
@@ -35,7 +35,7 @@ class Channels(HPTestCase):
 		self.assertEqual('%s/channels/view/%d/' % (URL_BASE, ID_USER_VIEW)	, channel.e('a.name').get_attribute('href'))
 		
 		h2 = self.e('.search-channels .right a')
-		self.assertEqual('Return to Featured Channels'	, h2.text)
+		self.assertEqual('Return to Featured Profiles'	, h2.text)
 		self.assertEqual('%s/channels/'	% URL_BASE		, h2.get_attribute('href'))
 	
 	@url('/channels/')
@@ -70,6 +70,6 @@ class Channels(HPTestCase):
 		self.assertEqual('%s/channels/view/%d/' % (URL_BASE, ID_USER_VIEW), channel.e('a.name').get_attribute('href'))
 		
 		h2 = self.e('.search-channels .right a')
-		self.assertEqual('Return to Featured Channels'	, h2.text)
+		self.assertEqual('Return to Featured Profiles'	, h2.text)
 		self.assertEqual('%s/channels/'	% URL_BASE, h2.get_attribute('href'))
 	
