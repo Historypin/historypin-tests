866b2f38bfda029691abb6cabdff30d114703f11
Gabriela Ananieva <g.ananieva@avalith.bg>
2014-05-26 14:05:27 +0300

    Suggestions.test_specific_date_suggestion

--- cases/suggestions.py
+++ cases/suggestions.py
@@ -40,22 +40,50 @@ class Suggestions(HPTestCase):
 		alert.accept()
 		
 		self.assertFalse(title_suggestion, WebElement)
+		
 		# TODO refactor this when suggestions are on the map
 		pass
 	
-	@url('/%d' % ID_MAP_ITEM)
+	@unittest.expectedFailure
+	@url('dialog/%d/tab:write-story/suggest:date/' % ID_MAP_ITEM)
 	def test_specific_date_suggestion(self):
+		
+		sp_date_option = self.e('#suggestion_field option:nth-of-type(3)')
+		self.assertTrue(sp_date_option.is_selected())
+		self.assertEqual('Date', sp_date_option.text)
+		
+		self.e('#day option:nth-of-type(4)').click()
+		self.e('#month option:nth-of-type(10)').click()
+		self.e('#year option:nth-of-type(4)').click()
+		
+		# TODO check if it is correct
+		self.assertEqual('disabled', self.e('#year_from').get_attribute('class'))
+		self.assertEqual('disabled', self.e('#year_to').get_attribute('class'))
+		
+		self.e('.apply').click()
+		
+		alert = self.browser.switch_to_alert()
+		alert.accept()
+		
+		self.e('.write_story').send_keys('I think this is the right specific date')
+		self.e('.apply').click()
 		# TODO
-		# open link /tab:write-story/suggest:date/
-		# check if date option is selected
-		# select each option for date, month, and year
-		# check if the suggest a timefrime has class disabled
-		# click publish
-		# accept the allert
-		# enter a description
-		# click publish
-		# check the suggestion in comments
-		# delete the suggestion
+		
+		sp_date_suggestion = ('.stories_list .suggestion.date')
+		self.assertEqual(self.e('#photo-side .photo-date').text		, sp_date_suggestion.e('.ba_from .value').text)  # to compare if the title is equal to photo title
+		self.assertEqual('3 September 2012'							, sp_date_suggestion.e('.ba_to .value').text)
+		self.assertEqual('I think this is the right specific date'	, sp_date_suggestion.e('.story_cnt').text)
+		
+		self.assertTrue(self.e('.suggestion-accept'), WebElement)
+		self.assertTrue(self.e('.icon.delete'), WebElement)
+		
+		self.e('.icon.delete').click()
+		alert = self.browser.switch_to_alert()
+		alert.accept()
+		
+		self.assertFalse(sp_date_suggestion, WebElement)
+		
+		# TODO refactor this when suggestions are on the map
 		pass
 	
 	@url('/%d' % ID_MAP_ITEM)
@@ -70,6 +98,8 @@ class Suggestions(HPTestCase):
 		# enter a description
 		# check the suggestion in comments
 		# delete the suggestion
+		
+		# TODO refactor this when suggestions are on the map
 		pass
 	
 	@url('/%d' % ID_MAP_ITEM)
@@ -84,6 +114,8 @@ class Suggestions(HPTestCase):
 		# click publish
 		# assert the comment section in the dialog
 		# delete the suggestion
+		
+		# TODO refactor this when suggestions are on the map
 		pass
 	
 	@url('/%d' % ID_MAP_ITEM)
@@ -102,6 +134,8 @@ class Suggestions(HPTestCase):
 		# enter a description
 		# check the suggestion section
 		# delete the suggestion
+		
+		# TODO refactor this when suggestions are on the map
 		pass
 	
 	@url('/%d' % ID_MAP_ITEM)
@@ -116,5 +150,7 @@ class Suggestions(HPTestCase):
 		# enter a description
 		# check the suggestion section
 		# delete the suggestion
+		
+		# TODO refactor this when suggestions are on the map
 		pass
 	
