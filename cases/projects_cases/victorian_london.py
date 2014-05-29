fdc5e97dd0efd8287db2816ab249518c2efc26bb
Gabriela Ananieva <g.ananieva@avalith.bg>
2014-05-29 11:24:26 +0300

    Project_VictorianLondon.test_index and Project_VictorianLondon.test_about

--- cases/projects_cases/victorian_london.py
+++ cases/projects_cases/victorian_london.py
@@ -9,20 +9,41 @@ class Project_VictorianLondon(HPTestCase):
 	
 	def test_index(self):
 		self.go(self.PROJECT_URL)
-		# TODO
-		# assert title
-		# assert in text
-		# assert link
-		# assert headings, links and texts for projects
-		# assert logo
+		
+		project_cnt = self.e('.landing-cnt')
+		
+		self.assertEqual('Mapping emotions in Victorian London', project_cnt.e('h2').text)
+		self.assertIn('Mapping the Emotions of London is a unique crowdsourcing experiment', project_cnt.e('.desc').text)
+		
+		
+		projects_items = [
+				['Dreadful London'						, '/dreadful-london/'],
+				['London in the light'					, '/dreadful-london/'],
+				['A day in the life of the old London'	, '/dreadful-london/'],
+		]
+		
+		project_section	= project_cnt.e('section')
+		h4s				= project_section.es('.item h4')
+		link_pr			= projects_section.es('.item a')
+		
+		for n in range(len(projects_items)):
+			i = projects_items[n]
+			self.assertEqual(i[0], h4s[n].text)
+			self.assertEqual(URL_BASE + self.PROJECT_URL + i[1], link_pr[n].get_attribute('href'))
+		
+		self.assertEqual('The Andrew W. Mellon Foundation', self.e('.partnership a').text)
+		
+		# TODO refactor this when there is text and links
 		pass
 	
 	def test_about(self):
 		self.go(self.PROJECT_URL)
-		# TODO
-		# click link find out more
-		# see if about title is on the page
-		# click go back
-		# see if find out more is on the page
-		pass
+		
+		self.e('.desc a').click()
+		
+		self.assertEqual('About', self.e('.about h3').text)
+		
+		self.e('.about a').click()
+		
+		self.assertTrue(self.e('.desc a'), WebElement)
 	
