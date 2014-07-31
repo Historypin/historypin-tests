7107729c7291ffbc323bf12fe1127be9ffd69cc1
Gabriela Ananieva <g.ananieva@avalith.bg>
2014-07-31 11:15:29 +0300

    first_world_war.py and digital_war_memorial.py files created

---
+++ cases/projects_cases/digital_war_memorial.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from base import *
+import os, sys
+
+class Project_FirstWorldWar(HPTestCase):
+	
+	PROJECT_URL = '/en/explore/the-digital-war-memorial'
+	
+	def test_index(self):
+		self.go(self.PROJECT_URL)
+		pass
