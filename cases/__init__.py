103b8478fff5d1a4e30cd543ea45c56809865569
Gabriela Ananieva <g.ananieva@avalith.bg>
2015-07-24 15:54:07 +0300

    Test Pages_View.test_homepage_view

--- cases/__init__.py
+++ cases/__init__.py
@@ -4,4 +4,4 @@ from explore.project_edit import Project_Edit
 from explore.people import People
 from explore.places import Places
 from explore.projects import Projects
-
+from explore.views import Pages_View
---
+++ cases/explore/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from base import *
+import os, sys
+
+class Pages_View(HPTestCase):
+	
+	# TODO put in order test later to be in seperate files
+	# TODO These later could be extended to fully check the content and/or functionality.
+	
+	@url('{0}/en/'.format(URL_BASE))
+	def test_homepage_view(self):
+		
+		self.assertTitle('Historypin | Home')
+		
+		self.assertIsInstance(self.e('#main-header'), WebElement)
+		
+		page_intro = self.e('.page-intro')
+		self.assertEqual('{0}/resources/explore/images/homepage-intro-image.jpg'.format(URL_BASE), page_intro.e('img').get_attribute('src'))
+		self.assertEqual('Working Together To\nRemember Our History', page_intro.e('h1').text)
+		
+		self.assertIsInstance(self.e('.partners'), WebElement)
+		
+		# assert all sections
