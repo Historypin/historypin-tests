bb90f1684b6cf52e60c8af99b08aea87a8613e2c
Gabriela Ananieva <g.ananieva@avalith.bg>
2015-09-01 15:14:10 +0300

    People.test_index created

--- cases/explore/people.py
+++ cases/explore/people.py
@@ -4,5 +4,22 @@ from base import *
 import os, sys
 
 class People(HPTestCase):
-	pass
+	
+	@url('/en/people')
+	def test_index(self):
+		
+		self.assertTitle('Historypin | People')
+		# TODO
+		# assert title
+		# assert collections and places links
+	
+	
+	# @url('')
+	# def test_most_popular(self):
+		
+	# 	# assert if the option menu is ordered by most popular
+	# 	# check if the first profile is most viewed
+	# 	pass
+	
+	
 	
